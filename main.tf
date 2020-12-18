8b7fdd6f890db288faf22fdc6b86d98c5652c502
stekern <stekern@users.noreply.github.com>
2020-12-18 09:49:49 +0100

    Rename and move local variables

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  current_account_id = data.aws_caller_identity.current.account_id
-  current_region     = data.aws_region.current.name
-  state_machine_name = "${var.name_prefix}-state-machine"
+  current_account_id    = data.aws_caller_identity.current.account_id
+  current_region        = data.aws_region.current.name
+  default_state_timeout = 3600
 }
 
 resource "aws_sfn_state_machine" "this" {
   name       = local.state_machine_name
-  definition = jsonencode(local.state_definition)
+  definition = jsonencode(local.state_machine_definition)
   role_arn   = aws_iam_role.this.arn
   tags       = var.tags
 }
